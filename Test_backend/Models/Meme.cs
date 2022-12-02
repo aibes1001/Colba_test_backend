3c88d1591f9c0534746852bff55d9936a18f094c
Aitor <aibes1001@gmail.com>
2022-12-02 11:01:46 +0100

    Created Meme Model

---
+++ Test_backend/Models/Meme.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Test_backend.Models
+{
+    public class Meme
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+
+        public string Original { get; set; }
+
+        public string Thumbnail { get; set; }
+
+    }
+}
