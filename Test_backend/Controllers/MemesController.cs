e4450cd2eceb7c408cebabefac2eaf2a69bf0119
Aitor Benítez Estruch <aitor.benitez@colba.es>
2022-12-05 13:15:00 +0100

    Exception handler implemented in MemeController

--- Test_backend/Controllers/MemesController.cs
+++ Test_backend/Controllers/MemesController.cs
@@ -15,75 +15,124 @@ namespace Test_backend.Controllers
         }
 
         [HttpGet]
-        public async Task<List<Meme>> GetAll() =>
-            await _memesService.GetAsync();
+        public async Task<ActionResult<List<Meme>>> GetAll()
+        {
+            try
+            {
+                return await _memesService.GetAsync();
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+            
+        }
 
         //Search an id with 24 caracters!!
         [HttpGet("id/{id:length(24)}")]
         public async Task<ActionResult<Meme>> GetById(string id)
         {
-            var meme = await _memesService.GetIdAsync(id);
-
-            if (meme is null)
+            try
             {
-                return NotFound();
-            }
+                var meme = await _memesService.GetIdAsync(id);
 
-            return meme;
+                if (meme is null)
+                {
+                    return NotFound();
+                }
+
+                return meme;
+            }
+            catch(Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+            
         }
 
 
         [HttpGet("name/{name}")]
         public async Task<ActionResult<Meme>> GetByName(string name)
         {
-            var meme = await _memesService.GetNameAsync(name);
-
-            if (meme is null)
+            try
             {
-                return NotFound();
-            }
+                var meme = await _memesService.GetNameAsync(name);
 
-            return meme;
+                if (meme is null)
+                {
+                    return NotFound();
+                }
+
+                return meme;
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+            
         }
 
         [HttpPost]
         public async Task<IActionResult> Post(Meme newMeme)
         {
-            await _memesService.CreateAsync(newMeme);
+            try
+            {
+                await _memesService.CreateAsync(newMeme);
 
-            return CreatedAtAction(nameof(GetById), new { id = newMeme.Id }, newMeme);
+                return CreatedAtAction(nameof(GetById), new { id = newMeme.Id }, newMeme);
+            }
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+            
         }
 
         [HttpPut("{id:length(24)}")]
         public async Task<IActionResult> Update(string id, Meme updatedMeme)
         {
-            var meme = await _memesService.GetIdAsync(id);
-
-            if (meme is null)
+            try
             {
-                return NotFound();
+                var meme = await _memesService.GetIdAsync(id);
+
+                if (meme is null)
+                {
+                    return NotFound();
+                }
+
+                updatedMeme.Id = meme.Id;
+
+                await _memesService.UpdateAsync(id, updatedMeme);
+
+                return NoContent();
             }
-
-            updatedMeme.Id = meme.Id;
-
-            await _memesService.UpdateAsync(id, updatedMeme);
-
-            return NoContent();
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+            
         }
 
         [HttpDelete("{id:length(24)}")]
         public async Task<IActionResult> Delete(string id)
         {
-            var meme = await _memesService.GetIdAsync(id);
-
-            if (meme is null)
+            try
             {
-                return NotFound();
+                var meme = await _memesService.GetIdAsync(id);
+
+                if (meme is null)
+                {
+                    return NotFound();
+                }
+
+                await _memesService.RemoveAsync(id);
+
+                return NoContent();
             }
-
-            await _memesService.RemoveAsync(id);
-
-            return NoContent();
+            catch (Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
 
         }
 
@@ -91,23 +140,30 @@ namespace Test_backend.Controllers
 
         //Implementation a paginator
         [HttpGet("search")]
-        public async Task<SearchByNamePaginationResponse> SearchByNameAsync([FromQuery] SearchByNameDescriptionFilter filter)
+        public async Task<ActionResult<SearchByNamePaginationResponse>> SearchByNameAsync([FromQuery] SearchByNameDescriptionFilter filter)
         {
-            var memesListed = new List<Meme>();
-            var memesFiltered = await _memesService.PaginationFilteredByNameDescription(filter);
-
-            for (var i = 0; i <= filter.ElementsInPageCount - 1; i++)
+            try
             {
-                var element = memesFiltered.ElementAtOrDefault(filter.ElementsInPageCount * (filter.Page - 1) + i);
-                if (element != null) memesListed.Add(element);
+                var memesListed = new List<Meme>();
+                var memesFiltered = await _memesService.PaginationFilteredByNameDescription(filter);
+
+                for (var i = 0; i <= filter.ElementsInPageCount - 1; i++)
+                {
+                    var element = memesFiltered.ElementAtOrDefault(filter.ElementsInPageCount * (filter.Page - 1) + i);
+                    if (element != null) memesListed.Add(element);
+                }
+
+
+                var total = (double)memesFiltered.Count / filter.ElementsInPageCount;
+                var totalPages = (int)Math.Ceiling(total);
+
+                return new SearchByNamePaginationResponse(filter.Page, totalPages, filter.ElementsInPageCount, memesListed);
             }
-
-
-            var total = (double)memesFiltered.Count / filter.ElementsInPageCount;
-            var totalPages = (int)Math.Ceiling(total);
-
-            return new SearchByNamePaginationResponse(filter.Page, totalPages, filter.ElementsInPageCount, memesListed);
-
+            catch(Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Database failure");
+            }
+            
         }
 
 
